7bd46ac60557e55f1eed6818df5616ac0215c982
20amove <20amoratie@gmail.com>
2023-06-29 04:18:57 -0400

    Another pass at outline shader

---
+++ GameConcept-E-N/Assets/Shaders/GetCrossSampleUVs.hlsl
@@ -0,0 +1,37 @@
+void GetCrossSampleUVs_float(float4 UV, float2 TexelSize,
+	float OffsetMultiplier, out float2 UVOriginal,
+	out float2 UVTopRight, out float2 UVBottomLeft,
+	out float2 UVTopLeft, out float2 UVBottomRight) {
+
+	UVOriginal = UV;
+	UVTopRight = UV.xy + float2(TexelSize.x, TexelSize.y) * OffsetMultiplier;
+	UVBottomLeft = UV.xy - float2(TexelSize.x, TexelSize.y) * OffsetMultiplier;
+	UVTopLeft = UV.xy + float2(-TexelSize.x * OffsetMultiplier, TexelSize.y * OffsetMultiplier);
+	UVBottomRight = UV.xy + float2(TexelSize.x * OffsetMultiplier, -TexelSize.y * OffsetMultiplier);
+
+}
+
+//UVOriginal = UV;
+//UVTopRight = UV.xy + float2(TexelSize.x, TexelSize.y) * OffsetMultiplier;
+//UVBottomLeft = UV.xy - float2(TexelSize.x, TexelSize.y) * OffsetMultiplier;
+//UVTopLeft = UV.xy + float2(-TexelSize.x * OffsetMultiplier, TexelSize.y * OffsetMultiplier);
+//UVBottomRight = UV.xy + float2(TexelSize.x * OffsetMultiplier, -TexelSize.y * OffsetMultiplier);
+
+//void GetNearbyEdges_float(float Val, float2 TexelSize, float OutlineThickness,
+//	out float Out) {
+//
+//	float TR;
+//	float BL;
+//	float TL;
+//	float BR;
+//
+//	for (int i = 1; i < OutlineThickness; i++) {}
+//		if (Val > .5) {
+//			TR = Val + float2(TexelSize.x, TexelSize.y) * i;
+//			TR = Val - float2(TexelSize.x, TexelSize.y) * i;
+//			TR = Val + float2(-TexelSize.x * i, TexelSize.y * i);
+//			TR = Val + float2(TexelSize.x * i, -TexelSize.y * i);
+//		}
+//	}
+//
+//}
