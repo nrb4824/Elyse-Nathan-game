d532cc1e8c8e2b8c1a0e4846ea75a569c3149b04
Nathan Borkholder <78773812+nrb4824@users.noreply.github.com>
2023-07-31 18:24:42 -0400

    Revert "help"
    This reverts commit 7ad7a228243189f37521e78d8e5eb5c069c17210.

--- GameConcept-E-N/Assets/Scripts/StateManager.cs
+++ GameConcept-E-N/Assets/Scripts/StateManager.cs
@@ -6,20 +6,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class StateManager : MonoBehaviour
 {
-    public static StateManager instance;
-    void Awake()
+    public void Awake()
     {
-        if (instance == null)
-        {
-            instance = this;
-
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-        DontDestroyOnLoad(gameObject);
     }
     public void ReloadCurrentScene()
     {
--- GameConcept-E-N/Assets/Scripts/UIManager.cs
+++ GameConcept-E-N/Assets/Scripts/UIManager.cs
@@ -5,45 +5,14 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    private GameObject deathPanel;
-    private GameObject winPanel;
-
-    public static UIManager instance;
-
-    void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-        DontDestroyOnLoad(gameObject);
-
-        /*if (GameObject.FindGameObjectWithTag("DeathPanel") != null)
-        {
-            Debug.Log("working1");
-            
-        }
-        if (GameObject.FindGameObjectWithTag("WinPanel") != null)
-        {
-            Debug.Log("working2");
-            
-        }*/
-
-    }
+    [SerializeField] GameObject deathPanel;
+    [SerializeField] GameObject winPanel;
     public void ToggleDeathPanel()
     {
-        deathPanel = GameObject.FindGameObjectWithTag("DeathPanel");
         deathPanel.SetActive(!deathPanel.activeSelf);
     }
     public void ToggleWinPanel()
     {
-        winPanel = GameObject.FindGameObjectWithTag("WinPanel");
         Console.WriteLine("Pannel");
         winPanel.SetActive(!winPanel.activeSelf);
     }
